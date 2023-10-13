1b457b230a4af2511ea7f16cdd93ed78724178ba
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2023-10-13 22:25:08 +0530

    Added smooth scrolling and bit intro of page 2

--- script.js
+++ script.js
@@ -1,4 +1,44 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
+gsap.registerPlugin(ScrollTrigger);
+
+
+const locoScroll = new LocomotiveScroll({
+el: document.querySelector(".main"),
+smooth: true
 });
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".main", {
+scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+}, 
+getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+},
+pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+
+var tl=gsap.timeline({
+    scrollTrigger:{
+        trigger:".page1 h1",
+        scroller:".main",
+        markers:true,
+        start:"top 27%",
+        end:"top 0",
+        scrub:3
+    }
+})
+tl.to(".page1 h1",{
+    x:-100,
+},"anim")
+
+tl.to(".page1 h2",{
+    x:100
+},"anim")
+
+tl.to(".page1 video",{
+    width:"90%"
+},"anim")
