0eb9058b17f1e50662ad9f5d45179665c49542bb
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2023-10-15 16:18:00 +0530

    Image change and smooth scroll fixed

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-function init(){
+function init() {
     gsap.registerPlugin(ScrollTrigger);
 
-const locoScroll = new LocomotiveScroll({
-el: document.querySelector(".main"),
-smooth: true
-});
-locoScroll.on("scroll", ScrollTrigger.update);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector(".main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
 
-ScrollTrigger.scrollerProxy(".main", {
-scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-}, 
-getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-},
-pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.scrollerProxy(".main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    });
 
-ScrollTrigger.refresh();
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    ScrollTrigger.refresh();
+
 }
 
 init()
@@ -43,7 +46,7 @@ var tl=gsap.timeline({
         trigger:".page1 h1",
         scroller:".main",
         // markers:true,
-        start:"top 27%",
+        start:"top 17%",
         end:"top 0",
         scrub:3
     }
@@ -64,9 +67,9 @@ var tl2=gsap.timeline({
     scrollTrigger:{
         trigger:".page1 h1",
         scroller:".main",
-        // markers:true,
+        markers:true,
         start:"top -115%",
-        end:"top 120%",
+        end:"top -120%",
         scrub:3
     }
 })
@@ -79,8 +82,8 @@ var tl3=gsap.timeline({
     scrollTrigger:{
         trigger:".page1 h1",
         scroller:".main",
-        // markers:true,
-        start:"top -280%",
+        markers:true,
+        start:"top -190%",
         end:"top -300%",
         scrub:3
     }
