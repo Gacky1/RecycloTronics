7cca6c029ce803629f1cee0fd4ec13a4d881a585
Gacky1 <rachitavasthi.it25@jecrc.ac.in>
2023-10-17 11:08:04 +0530

    Login issue tried to fixed

--- Login/script.js
+++ Login/script.js
@@ -9,3 +9,32 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
+document.getElementById('myButton').addEventListener('click', function() {
+    // Get the values from the text fields
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Check if both text fields are filled
+    if (username && password) {
+      // Redirect to the desired link when the button is clicked
+      window.location.href = './Loader/index.html';
+    } else {
+      // Display an alert or perform other actions if the fields are not filled
+      alert('Please fill in both fields before clicking the button.');
+    }
+  });
+
+  // Enable the button only when both text fields are filled
+  document.getElementById('username').addEventListener('input', toggleButtonState);
+  document.getElementById('password').addEventListener('input', toggleButtonState);
+
+  function toggleButtonState() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var button = document.getElementById('myButton');
+    
+    // Enable the button only if both text fields are filled
+    button.disabled = !(username && password);
+  }
